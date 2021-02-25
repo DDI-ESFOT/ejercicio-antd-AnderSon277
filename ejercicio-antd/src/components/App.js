0be7aa2df4840c8226db3b0d0b895bfeb9c46fcc
AnderSon277 <anderson.cordova@epn.edu.ec>
2021-02-24 23:35:24 -0500

    Third commit

--- ejercicio-antd/src/components/App.js
+++ ejercicio-antd/src/components/App.js
@@ -2,6 +2,8 @@ import '../styles/App.css';
 import {useState, useEffect } from "react";
 import {Input,Col,Row} from 'antd';
 import ListMovies from './ListMovies';
+
+
 function App() {
 
   const [movies, setMovies] = useState([]);
@@ -10,14 +12,12 @@ function App() {
 
   useEffect(() => {
     const getData = async () => {
-      if(search!==""){
+      if(search){
         const response = await fetch(
-          `https://www.omdbapi.com/?i=tt3896198&apikey=e23801d0&s=${search}`
+          `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${search}`
         );
         const data = await response.json();
         console.log("Data", data);
-        const results = data.Search; 
-        console.log("Search",results);
         setMovies( data.Search);
         console.log("SetMovies",movies);
       }
@@ -46,11 +46,11 @@ function App() {
     <>
     <Row>
       <Col span={24} style={searchStyle}>
-        <Input.Search id="movie" placeholder="Ingrese frase o palabra: " value={element} defaultValue={element} onChange={(e)=>{console.log('e', e.target.value );setElement(e.target.value)}} enterButton onSearch={onSearch} onPressEnter={onSearch} style={{width:250}}/>
+        <Input.Search id="movie" placeholder="Ingrese frase o palabra: " value={element} defaultValue={element} onChange={(e)=>{console.log('e', e.target.value );setElement(e.target.value)}} enterButton onSearch={onSearch} style={{width:250}}/>
       </Col>
     </Row>
     <br></br>
-    {movies.length > 0 ? <ListMovies movies={movies}/>: ("NO HAY RESULTADOS")}
+    {movies ? <ListMovies movies={movies}/>: ("NO HAY RESULTADOS PARA MOSTRAR")}
     </>
   );
 }
--- ejercicio-antd/src/components/ListMovies.js
+++ ejercicio-antd/src/components/ListMovies.js
@@ -1,9 +1,37 @@
-import React from 'react'
-import {Card,Row, Col} from 'antd';
-import LearMore from './LearMore';
+import {useState, useEffect } from "react";
+import {Card, Row, Col, Button, Modal, Image, Rate} from 'antd';
 
 const ListMovies = ({movies}) => {
-    
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [id, setId] = useState(null);
+    const [dataMovie, setDataMovie] = useState(null);
+  
+    useEffect(() => {
+      const getData = async () => {
+        if (id) {
+          const response = await fetch(
+            `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&i=${id}`
+          );
+          const data = await response.json();
+          console.log("data", data);
+          setDataMovie(data);
+        }
+      };
+  
+      getData();
+    }, [id]);
+
+    const handleClose = () => {
+        setIsModalVisible(false);
+      };
+
+    const handleViewMore = (movieId) => {
+        console.log("movieId", movieId);
+        setId(movieId);
+        setIsModalVisible(true);
+      };
+
     return (
     <>
     <Row style={{ margin: '0 10%'}}>
@@ -18,7 +46,12 @@ const ListMovies = ({movies}) => {
             />
             }
             actions={[
-            <LearMore/>
+                <Button
+                  type="link"
+                  onClick={() => handleViewMore(movie.imdbID)}
+                >
+                  Ver más
+                </Button>
             ]}
             >
             <Card.Meta
@@ -28,7 +61,37 @@ const ListMovies = ({movies}) => {
             </Card>
         </Col>
 	))}
-    </Row>  
+    </Row>
+    {dataMovie ?     
+    <Row>
+    <Modal
+        title={dataMovie.Title} 
+        visible={isModalVisible} 
+        onCancel={handleClose}
+        onOk={handleClose}
+        >
+        {<Row>
+            <Col span={12} >
+            <p ><strong>Titulo original: </strong>{dataMovie.Title}</p>
+            <p ><strong>Fecha de estreno: </strong>{dataMovie.Released}</p>
+            <p ><strong>Idioma/s: </strong> {dataMovie.Language}</p>
+            <p ><strong>Genero/s: </strong> {dataMovie.Genre}</p>
+            <p ><strong>Ciudad deorigen: </strong>{dataMovie.Country}</p>
+            <p ><strong>Director/es: </strong>{dataMovie.Director}</p>
+            <p ><strong>Escritor/es" </strong>{dataMovie.Writer}</p>
+            <p ><strong>Actor/es: </strong>{dataMovie.Actors}</p>
+            <p ><strong>Raiting: </strong><Rate disabled defaultValue={dataMovie.imdbRating} /></p>
+            </Col>
+            <Col span={12}>
+                <Image
+                height="fit-content"
+                src={dataMovie.Poster}
+                />
+            </Col>
+        </Row>}
+    </Modal>
+    </Row>  : ("")
+    }
     </>
     )
 }
